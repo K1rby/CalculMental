46e2d21a4f4dccc0a4065cb8aed8a924f7b33e4f
K1rby <quentin.fontenay@hotmail.com>
2019-10-23 18:06:18 +0200

    connexion fonctionnel

---
+++ src/dal/IUserDAO.java
@@ -0,0 +1,10 @@
+package dal;
+
+import bo.User;
+
+import java.sql.SQLException;
+
+public interface IUserDAO<ID, E> extends IDAO<ID, E> {
+	
+	public User authenticate( String login, String password ) throws SQLException;
+}
